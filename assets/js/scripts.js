4de905db0551d3c6f33425df4592f77e46162021
Alex <alex_p97@live.com>
2025-01-09 23:18:43 +0000

    Modified JS to also add padding-top

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,12 +1,16 @@
 //If the page is loaded on a mobile device, fix navbar to top. Use Bootstrap breakpoints.
 function checkViewportWidth() {
+    
     const navbar = document.querySelector('.navbar');
+    const body = document.body;
+    
     if (window.innerWidth <= 1200) {
         navbar.classList.add('fixed-top');
+        body.style.paddingTop = '58px';
     } 
-    
     else {
         navbar.classList.remove('fixed-top');
+        body.style.paddingTop = '0';
     }
 }
 
